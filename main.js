4d3b4d56f9808d7a7f469677063081fd24a78b45
Jqnkun <huuki.0509@outlook.jp>
2023-12-10 23:57:36 +0900

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,25 @@
-let maxinum = prompt('数字を入力してください');
+let maxinum = parseInt(prompt('数字を入力してください'));
+while(!maxinum){
+    maxinum = parseInt(prompt('エラーが起きました。有効な数字を入力してください。'))
+}
+
+const targetNum = Math.floor(Math.random() * maxinum) + 1;
+// console.log(targetNum);
+
+let guess = prompt('数字を当ててください。');
+let count = 1;
+while(parseInt(guess) !== targetNum){
+    if (guess === 'q') break;
+    count++;
+    if(guess > targetNum){
+        guess = parseInt(prompt('その数字より小さい。もう一度'));
+    }else {
+        guess = parseInt(prompt('その数字より大きい。もう一度'));
+    }
+}
+
+if(guess === 'q'){
+    console.log('終了');
+} else {
+    console.log(`${count}回で正解`);
+}
